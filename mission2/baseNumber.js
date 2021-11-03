74f7b68a955ba9952bcab68133178f232bced675
Leejin-Yang <ejin1996@gmail.com>
2021-11-03 23:29:03 +0900

    말해야 하는 숫자 출력 구현

--- mission2/baseNumber.js
+++ mission2/baseNumber.js
@@ -1,12 +1,30 @@
-function solution(base, number, playerNum) {
+function solution(base, number, totalPlayer) {
   let valueBase;
   const numArr = [];
+  const range = number * totalPlayer;
 
-  for (let value = 0; value < number * playerNum; value++) {
+  for (let value = 0; value < range; value++) {
     valueBase = value.toString(base).split("");
     numArr.push(...valueBase);
   }
   return numArr;
 }
 
-console.log(solution(2, 4, 2));
+function getNumberToSay(base, number, totalPlayer, orderNum) {
+  let index = orderNum - 1;
+  const orderNumArr = [];
+  const numArr = solution(base, number, totalPlayer);
+
+  while (index < numArr.length) {
+    orderNumArr.push(numArr[index]);
+    index += totalPlayer;
+  }
+  return orderNumArr;
+}
+
+function testCase() {
+  console.log(solution(3, 4, 2));
+  console.log(getNumberToSay(3, 4, 2, 1));
+}
+
+testCase();
