97f269d49772fe2d4da4fa8151f5562c71632ec7
Leejin-Yang <ejin1996@gmail.com>
2021-11-08 22:16:01 +0900

    convertHashStringToInt, put, get 함수 구현

---
+++ day3/hashMap.js
@@ -0,0 +1,35 @@
+function convertHashStringToInt(key, tableSize) {
+  let hashCode = 17;
+
+  for (let i = 0; i < key.length; i++) {
+    hashCode = (hashCode * key.charCodeAt(i)) % tableSize;
+  }
+  return hashCode;
+}
+
+function HashMap() {
+  this.table = new Array(17);
+
+  HashMap.prototype.put = function (key, value) {
+    const index = convertHashStringToInt(key, this.table.length);
+    this.table[index] = value;
+  };
+
+  HashMap.prototype.remove = function () {};
+  HashMap.prototype.containKey = function () {};
+
+  HashMap.prototype.get = function (key) {
+    const index = convertHashStringToInt(key, this.table.length);
+    return this.table[index];
+  };
+
+  HashMap.prototype.isEmpty = function () {};
+  HashMap.prototype.keys = function () {};
+  HashMap.prototype.replace = function () {};
+  HashMap.prototype.size = function () {};
+  HashMap.prototype.clear = function () {};
+}
+
+const myTable = new HashMap();
+myTable.put("firstname", "tom");
+console.log(myTable.get("firstname"));
