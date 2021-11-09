83cda66fb9cc9f86a9ce2bb577fb225b09a36f71
Leejin-Yang <ejin1996@gmail.com>
2021-11-09 16:52:05 +0900

    indexOf -> findIndex

--- day3/hashMap.js
+++ day3/hashMap.js
@@ -22,8 +22,10 @@ function HashMap() {
 
   HashMap.prototype.remove = function (key) {
     const index = convertHashStringToInt(key, this.table.length);
-    const data = this.table[index].find((data) => data[0] === key);
-    this.table[index].splice(this.table[index].indexOf(data), 1);
+    this.table[index].splice(
+      this.table[index].findIndex((data) => data[0] === key),
+      1
+    );
   };
 
   HashMap.prototype.containKey = function () {};
@@ -59,6 +61,6 @@ console.log(myTable.get("firstname"));
 console.log(myTable.get("lastname"));
 console.log(myTable.get("age"));
 console.table(myTable.table);
-myTable.remove("firstname");
+myTable.remove("012");
 console.table(myTable.table);
 console.log(myTable.keys());
